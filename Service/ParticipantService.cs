db33c67ac0d069ca3926000b12d51535edf11798
ygx <1241749920@qq.com>
2020-11-06 15:18:06 +0800

    .

--- Service/ParticipantService.cs
+++ Service/ParticipantService.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using EventPlatFormVer4.Models;
+using System.Data;
+using System.Data.Common;
+using MySql.Data.MySqlClient;
 
 namespace EventPlatFormVer4.Service
 {
     public class ParticipantService
     {
-        public static bool Apply()
+        //报名，将PartiState改成0
+        public static void Apply()
         {
-            return false;//this is a push test
+            MySqlConnection connection = new MySqlConnection();
+            connection.Open();
+            string applySql = "SELECT * FROM events WHERE State=1";
+            MySqlDataAdapter dataAdapter = new MySqlDataAdapter(applySql,connection);
+            DataSet eventList = new DataSet();
+            dataAdapter.Fill(eventList);
         }
     }
 }
