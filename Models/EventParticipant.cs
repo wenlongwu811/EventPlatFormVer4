04375897df71670de8c2932e7ac06ee0fc4d8a6f
Sun Yuankai <ekey666@gmail.com>
2020-11-07 18:48:17 +0800

    Add EventParticipant.cs

---
+++ Models/EventParticipant.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace EventPlatFormVer4.Models
+{
+    public class EventParticipant
+    {
+        [Key]
+        public string Id { get; set; }
+        public int Index { get; set; }
+        public Participant Participant { get; set; }
+        public string ParticipantId { get; set; }
+        [ForeignKey("ParticipantId")]
+        public string ParticipantName { get => Participant != null ? Participant.Name : ""; }
+        public string Grade { get; set; }
+        public Event Event { get; set; }
+        public string EventId { get; set; }
+        [ForeignKey("EventId")]
+        public string EventName { get => Event != null ? Event.Name : "" ; }
+        // TODO: decide the State number
+        public int State { get; set; }
+
+        public EventParticipant() {
+            Id = Guid.NewGuid().ToString();
+        }
+
+        public EventParticipant(int index,  Event @event, Participant participant)
+        {
+            this.Index = index;
+            this.Event = @event;
+            this.Participant = participant;
+        }
+
+        public override string ToString()
+        {
+            return $"[No.:{Index},event:{EventName},participant:{ParticipantName},grade:{Grade},state:{State}]";
+        }
+
+        public override bool Equals(object obj)
+        {
+            var item = obj as EventParticipant;
+            return item != null &&
+                EventName == item.EventName &&
+                ParticipantName == item.ParticipantName;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -231495720;
+            hashCode = hashCode * -1525538493 + Index.GetHashCode();
+            hashCode = hashCode * -1525538493 + EqualityComparer<string>.Default.GetHashCode(ParticipantName);
+            hashCode = hashCode * -1525538493 + EqualityComparer<string>.Default.GetHashCode(EventName);
+            return hashCode;
+        }
+    }
+}
---
+++ Service/EventService.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EventPlatFormVer4.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventPlatFormVer4.Service
+{
+    /**
+     * The service class to manage events
+     */
+    class EventService
+    {
+        public EventService(MvcEpfContext context) // context 配置
+        {
+            _context = context;
+        }
+
+        private static MvcEpfContext _context;
+
+        public static List<Event> GetAllEvents()
+        {
+            using (var db = _context)
+            {
+                return AllEvents(db).ToList();
+            }
+        }
+
+        public static Event GetEvent(string id) // 根据EventID来查询
+        {
+            using (var db = _context)
+            {
+                return AllEvents(db).FirstOrDefault(e => e.Id == id); // FirstOrDefault方法返回序列中的第一个或者默认值
+            }
+        }
+
+        public static Event AddEvent(Event @event) // 添加new Event
+        {
+            try
+            {
+                using (var db = _context)
+                {
+                    db.Events.Add(@event);
+                    db.SaveChanges();
+                }
+                return @event;
+            }
+            catch (Exception e)
+            {
+                // TODO: 需要根据错误类型返回不同错误信息
+                throw new ApplicationException($"添加活动出错{e.Message}");
+            }
+        }
+
+        public static void RemoveEvent(string id)
+        {
+            try
+            {
+                using (var db = _context)
+                {
+                    var @event = db.Events.Include("Participants").Where(e => e.Id == id).FirstOrDefault();
+                    db.Events.Remove(@event);
+                    db.SaveChanges();
+                }
+            }
+            catch(Exception e)
+            {
+                // TODO: 需要根据错误类型返回不同错误信息
+                throw new ApplicationException($"删除活动出错{e.Message}");
+            }
+        }
+
+        public static void UpdateEvent(Event newEvent)
+        {
+            Remo
+        }
+        public static void RemoveParticipants(string eventId)
+        {
+            using (var db = _context)
+            {
+                var oldParticipants = db.Participants.Where(p => p.PartiEvent.Exists(e => e.Id.Equals(eventId))); // 对Participants表查询，返回其中有参与对应活动ID的oldparticipants
+                db.Participants.RemoveRange(oldParticipants);
+
+            }
+        }
+        private static IQueryable<Event> AllEvents(MvcEpfContext db) // 返回所有的 Event
+        {
+            return db.Events.Include(e => e.Participants.Select(p => p.ID)).Include("Sponsor"); // include 方法会返回和该字段相关的信息
+        }
+    }
+}
