c0ee45a42a78644f2c3999c43136a80b7ccaa823
wenlongwu811 <55539855+wenlongwu811@users.noreply.github.com>
2020-11-08 17:36:09 +0800

    View层
    View层

--- Models/EventParticipant.cs
+++ Models/EventParticipant.cs
@@ -21,21 +21,22 @@ namespace EventPlatFormVer4.Models
         [ForeignKey("EventId")]
         public string EventName { get => Event != null ? Event.Name : "" ; }
         // TODO: decide the State number
-        public int State { get; set; }//赛事报名状态，0/1/2/3表示待审核/报名成功/报名失败/取消
+        public int State { get; set; }
 
         public EventParticipant() {
             Id = Guid.NewGuid().ToString();
         }
 
-        public EventParticipant(Event @event, Participant participant)
+        public EventParticipant(int index,  Event @event, Participant participant)
         {
+            this.Index = index;
             this.Event = @event;
             this.Participant = participant;
         }
 
         public override string ToString()
         {
-            return $"[No.:{Id},event:{EventName},participant:{ParticipantName},grade:{Grade},state:{State}]";
+            return $"[No.:{Index},event:{EventName},participant:{ParticipantName},grade:{Grade},state:{State}]";
         }
 
         public override bool Equals(object obj)
@@ -49,7 +50,7 @@ namespace EventPlatFormVer4.Models
         public override int GetHashCode()
         {
             var hashCode = -231495720;
-            hashCode = hashCode * -1525538493 + Id.GetHashCode();
+            hashCode = hashCode * -1525538493 + Index.GetHashCode();
             hashCode = hashCode * -1525538493 + EqualityComparer<string>.Default.GetHashCode(ParticipantName);
             hashCode = hashCode * -1525538493 + EqualityComparer<string>.Default.GetHashCode(EventName);
             return hashCode;
